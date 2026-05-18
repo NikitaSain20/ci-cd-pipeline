bc65bab3c52107bec32382190278095519ee9293
NikitaSain20 <nikitasen664@gmail.com>
2026-05-18 23:22:51 +0530

    initializing the repo

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.get("/", (_req, res) => {
+  res.send("Hello For CI/CD with GitHub Actions!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
