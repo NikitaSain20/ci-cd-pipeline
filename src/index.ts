2a6f9cca739f761a874390eb39f25dcae03a8de6
NikitaSain20 <nikitasen664@gmail.com>
2026-06-04 17:43:26 +0530

    changes done

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+const a;
 app.get("/", (_req, res) => {
   res.send("Hello For CI/CD with GitHub Actions!");
 });
